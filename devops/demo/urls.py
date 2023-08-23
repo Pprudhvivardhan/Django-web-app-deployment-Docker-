84fc4f55374c4002f1a82aee8fcfc13fa8743c93
prudhvi vardhan <99749916+Pprudhvivardhan@users.noreply.github.com>
2023-08-23 14:11:58 +0530

    Update urls.py

--- devops/demo/urls.py
+++ devops/demo/urls.py
@@ -1,10 +1,6 @@
-from django.urls import path
+ from django.urls import path
 from . import views
-from .views import home_view
-
 
 urlpatterns = [
-    path('', views.home_view),
-    path('demo/', views.demo_view),
-    path('admin/', views.admin_view),
+    path('', views.home_view)
 ]
